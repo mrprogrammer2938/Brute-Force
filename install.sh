c1a4111bc8289f6245cbe715db6ccab199ca4320
Ms.programmer <78996423+msprogrammer2938@users.noreply.github.com>
2021-05-12 07:28:40 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+# This code write by Ms.nope
+clear
+echo "installing..."
+sleep 2
+echo 
+echo "
+
+░█▀▀█ █▀▀█ █──█ ▀▀█▀▀ █▀▀ ── █▀▀ █▀▀█ █▀▀█ █▀▀ █▀▀ 
+░█▀▀▄ █▄▄▀ █──█ ──█── █▀▀ ▀▀ █▀▀ █──█ █▄▄▀ █── █▀▀ 
+░█▄▄█ ▀─▀▀ ─▀▀▀ ──▀── ▀▀▀ ── ▀── ▀▀▀▀ ▀─▀▀ ▀▀▀ ▀▀▀"
+echo ""
+sudo apt install python3
+sudo apt install python
+sudo apt install pip-python3
+pip install --upgrade pip
+pip install requirments.txt
+chmod +x bruteforce
+sleep 1
+echo "
+Finish installing !
+
+usage: ./bruteforce
+"
+exit 1
